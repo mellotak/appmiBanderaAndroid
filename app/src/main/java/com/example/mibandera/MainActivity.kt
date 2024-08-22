182d181844228244f2a960acdace3c2bbc516fee
sunombre1 <suemail1@gmail.com>
2024-08-21 23:00:03 -0500

    Dos: Creando la App, miBandera

--- app/src/main/java/com/example/mibandera/MainActivity.kt
+++ app/src/main/java/com/example/mibandera/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.mibandera
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var descriptionTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        imageView = findViewById(R.id.imageView)
+        descriptionTextView = findViewById(R.id.descriptionTextView)
+
+        val btnAmarillo: Button = findViewById(R.id.btnAmarillo)
+        val btnAzul: Button = findViewById(R.id.btnAzul)
+        val btnRojo: Button = findViewById(R.id.btnRojo)
+
+        btnAmarillo.setOnClickListener {
+            imageView.setImageResource(R.drawable.amarillo)
+            descriptionTextView.text = "El color amarillo de la bandera de Colombia simboliza la riqueza del país."
+        }
+
+        btnAzul.setOnClickListener {
+            imageView.setImageResource(R.drawable.azul)
+            descriptionTextView.text = "Azul: Representa los vastos océanos y ríos de Colombia."
+        }
+
+        btnRojo.setOnClickListener {
+            imageView.setImageResource(R.drawable.rojo)
+            descriptionTextView.text = "Rojo: Simboliza la sangre derramada por los patriotas TK."
         }
     }
 }
